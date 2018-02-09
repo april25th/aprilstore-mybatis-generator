4209689b96bd6e5056ee1bf364607fb2dbba8b26
wendong <wendong.shang@ecloud.com.cn>
2018-02-09 11:58:08 +0800

    add project

---
+++ src/main/java/aprilstore/App.java
@@ -0,0 +1,13 @@
+package aprilstore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
